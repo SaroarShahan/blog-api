40caea0419db149917f03fa1ea162e37aa0a5db2
SaroarShahan <saroar.shahan@gmail.com>
2023-02-25 00:00:44 +0600

    Update post route

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -7,10 +7,11 @@ import {
   getPosts,
   updatePost,
 } from '~/controllers/postController';
+import verifyToken from '~/middleware/verifyToken';
 
 const router = Router();
 
-router.route('/').get(getPosts).post(createPost);
-router.route('/:id').get(getPost).put(updatePost).delete(deletePost);
+router.route('/').get(getPosts).post(verifyToken, createPost);
+router.route('/:id').get(getPost).put(verifyToken, updatePost).delete(verifyToken, deletePost);
 
 export default router;
