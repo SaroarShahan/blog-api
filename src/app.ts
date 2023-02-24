0baf80d5c9b5ae21dd7b28b94d7db2e6b90d7047
SaroarShahan <saroar.shahan@gmail.com>
2023-02-25 00:02:12 +0600

    Add auth route in middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import authRoutes from '~/routes/authRoutes';
 import postRoutes from '~/routes/postRoutes';
 import dbConnect from '~/config/dbConeect';
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/posts', postRoutes);
 
 app.listen(_PORT, () => {
